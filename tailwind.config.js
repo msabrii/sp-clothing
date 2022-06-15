91d85f4bf023b61f462e09d9bfc3f6db26ddd725
Sheikh Ameer Sabri <ameers@apadmi.com>
2022-06-15 12:10:20 +0100

    Add screen sizes in tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {}
+		screens: {
+			xs: '340px',
+			sm: '400px',
+			md: '480px',
+			lg: '700px',
+			xl: '860px',
+			'2xl': '1200px',
+			tablet: { min: '700px', max: '859px' },
+			mobile: { max: '699px' },
+		},
+		extend: {},
 	},
-	plugins: []
+	plugins: [],
 };
