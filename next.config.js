13f55c4b7b7cc96a557a95add87e32c3e9b489a5
Sheikh Ameer Sabri <ameers@apadmi.com>
2022-06-15 12:10:08 +0100

    Add image config in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	images: {
+		minimumCacheTTL: 3600,
+		domains: ['images.ctfassets.net', 'downloads.ctfassets.net'],
+		formats: ['image/avif', 'image/webp'],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
